a993026f781e716ff8a66a47d3273dc3b23ff55f
rand0mnpc <dparks1996@gmail.com>
2018-04-21 19:44:48 -0400

    started struct defs for inodes, and directories

--- asst3/assignment3/src/params.h
+++ asst3/assignment3/src/params.h
@@ -23,10 +23,42 @@
 // maintain bbfs state in here
 #include <limits.h>
 #include <stdio.h>
+
+/*
+ * inode structure containing metadata
+ * about file stored on disk
+ */
+typedef struct inode {
+	int       st_ino;     /* inode number */
+	uid_t     st_uid;     /* user ID of owner */
+        gid_t     st_gid;     /* group ID of owner */
+	off_t     st_size;    /* total size, in bytes */
+        blksize_t st_blksize; /* blocksize for file system I/O */
+	char*     data;	      /* pointer to actual data */
+} inode_t;
+
+/*
+ * representation of a filename to
+ * inode mapping 
+ */
+typedef struct hard_link {
+	char* filename;
+	int st_ino;			// inode number
+	struct hard_link* next; 	// next entry in a list of hard links
+} hard_link_t;
+
+/*
+ * directory represented as a 
+ * list of hard links
+ */
+typedef struct directory {
+	int num_links;			// number of hard links
+	hard_link_t* front;		// first entry in list of links		
+} dir;
+
 struct sfs_state {
     FILE *logfile;
     char *diskfile;
 };
 #define SFS_DATA ((struct sfs_state *) fuse_get_context()->private_data)
 
-#endif
